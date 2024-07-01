8e633cd0dd3299ac42a11e1179279808dfa11493
giordanofabrizii <giordanofabrizi@gmail.com>
2024-07-01 14:19:11 +0200

    added scaffolding

---
+++ index.php
@@ -0,0 +1,17 @@
+
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todo List</title>
+</head>
+<body>
+    
+</body>
+
+<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+
+</html>
